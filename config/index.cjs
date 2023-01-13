aa94d4abfae77144fe0baaf30fa9582666bc2c0c
Frower222 <112725659+Frower222@users.noreply.github.com>
2023-01-13 23:57:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oaFM26JvjpwzcUhg8lnaHXhXZcp0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	siXbJy4gWXH4BLGw-dG4LCtqklTwkRFXHVrbwm8mJ-c',
+      useTemplateId: 'wXyVpbGQ2dXF06tmJjRpnJ1tu79i9SVR1vtaVO5_k7M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-03',
       festivals: [
@@ -50,7 +50,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	siXbJy4gWXH4BLGw-dG4LCtqklTwkRFXHVrbwm8mJ-c',
+  CALLBACK_TEMPLATE_ID: 'wXyVpbGQ2dXF06tmJjRpnJ1tu79i9SVR1vtaVO5_k7M',
 
   CALLBACK_USERS: [
     {
