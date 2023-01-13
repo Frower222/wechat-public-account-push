49d8d42062a37fb0bcab3c05b1cfbb4f61196af1
Frower222 <112725659+Frower222@users.noreply.github.com>
2023-01-14 00:51:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,10 +20,8 @@ const USER_CONFIG = {
     {
       // 想要发送的人的名字
       name: '小熊宝宝',
-      name: '小熊宝宝1',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oaFM26JvjpwzcUhg8lnaHXhXZcp0',
-      id: 'oaFM26Ael9Jw6ipOJLEdZ9Sr3duU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'RN1jmxABCwlWSaPVV-xC0kr_trmG1L2VvRzSR9WoOC4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
